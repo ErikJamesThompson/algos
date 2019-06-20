39482a377eb3929207658db92f91dd121323e83d
ErikJamesThompson <erik.james.thompson@gmail.com>
2019-06-19 18:50:46 -0700

    adding percolation set up files too

---
+++ percolation/Percolation.java
@@ -0,0 +1,23 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class Percolation {
+
+    // create n-by-n grid, with all sites blocked
+    public Percolation(int n) {
+        
+    }
+
+    public void open(int row, int col)    // open site (row, col) if it is not open already
+
+    public boolean isOpen(int row, int col)  // is site (row, col) open?
+
+    public boolean isFull(int row, int col)  // is site (row, col) full?
+
+    public int numberOfOpenSites()       // number of open sites
+
+    public boolean percolates()              // does the system percolate?
+}
